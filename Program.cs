71d27c7eb46b39f07dcc5d4e99ce16657f8aecbe
OliWan9 <88452705+OliWanKenobi9@users.noreply.github.com>
2023-12-19 20:03:58 +0100

    Added remaining Figures

--- Program.cs
+++ Program.cs
@@ -19,10 +19,10 @@ switch(inputChoice)
         break;
     case "4":
     case "Triangle":
-        Console.WriteLine("du hast 4 gedrückt");
+        Triangle();
         break;
     default:
-        Console.WriteLine("Invalid Input.");
+        Console.WriteLine("Invalid");
         break;
 }
 
@@ -77,6 +77,9 @@ void Square()
             {
                 Console.WriteLine("Invalid");
             }
+            break;
+        default:
+            Console.WriteLine("Invalid");
             break;
     }
 }
@@ -153,6 +156,128 @@ void Rectangle()
                 Console.WriteLine("Invalid");
             }
             break;
+        default:
+            Console.WriteLine("Invalid");
+            break;
     }
 }
 
+void Circle()
+{
+    Console.WriteLine("What do you want to calculate? Area (1) | Scope (2) | Radius (3) | Diameter (4) ");
+    string circleChoice = Console.ReadLine().ToLower();
+    if (circleChoice == null)
+    {
+        Console.WriteLine("Invalid");
+    }
+    switch (circleChoice)
+    {
+        case "1":
+        case "area":
+            Console.WriteLine("Whats the radius of the circle?");
+            double circleRadius = double.Parse(Console.ReadLine());
+
+            double circleArea = 2 * Math.PI * circleRadius;
+            Console.WriteLine("The Area is: " + circleArea);
+            
+            if (circleRadius == null)
+            {
+                Console.WriteLine("Invalid");
+            }
+            break;
+        case "2":
+        case "scope":
+            Console.WriteLine("Whats the radius of the circle?");
+            circleRadius = double.Parse(Console.ReadLine());
+
+            double circleScope = 2 * Math.PI * circleRadius;
+            Console.WriteLine("The Scope is: " + circleScope);
+            if (circleRadius == null)
+            {
+                Console.WriteLine("Invalid");
+            }
+            break;
+        case "3":
+        case "radius":
+            Console.WriteLine("Whats the Scope of the circle?");
+            circleScope = double.Parse(Console.ReadLine());
+
+            circleRadius = circleScope / (2 * Math.PI);
+            Console.WriteLine("The Radius is: " + circleRadius);
+            if (circleScope == null)
+            {
+                Console.WriteLine("Invalid");
+            }
+            break;
+        case "4":
+        case "diameter":
+            Console.WriteLine("Whats the Scope of the circle?");
+            circleScope = double.Parse(Console.ReadLine());
+
+            double circleDiameter = circleScope / (2 * Math.PI) * 2;
+            Console.WriteLine("The Radius is: " + circleDiameter);
+            if (circleScope == null)
+            {
+                Console.WriteLine("Invalid");
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid");
+            break;
+    }
+}
+
+void Triangle()
+{
+    Console.WriteLine("What do you want to calculate? Area (1) | Sidelength C (2) | Scope (3)");
+    string triangleChoice = Console.ReadLine();
+    if (triangleChoice == null)
+    {
+        Console.WriteLine("Invalid");
+    }
+    switch (triangleChoice)
+    {
+        case "1":
+        case "area":
+            Console.WriteLine("What is the length of side A?");
+            double triangleSideA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the length of side B?");
+            double triangleSideB = double.Parse(Console.ReadLine());
+
+            double triangleSideC = Math.Sqrt(Math.Pow(triangleSideA, 2) + Math.Pow(triangleSideB, 2));
+            double triangleS = (triangleSideA + triangleSideB + triangleSideC) / 2;
+            double triangleArea = Math.Sqrt(triangleS * (triangleS - triangleSideA) * (triangleS - triangleSideB) * (triangleS - triangleSideC));
+            if (triangleSideA == null)
+            {
+                Console.WriteLine("Invalid");
+            }
+            if (triangleSideB == null)
+            {
+                Console.WriteLine("Invalid");
+            }
+            break;
+        case "2":
+        case "sidelength c":
+            Console.WriteLine("What is the length of side A?");
+            triangleSideA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the length of side B?");
+            triangleSideB = double.Parse(Console.ReadLine());
+
+            triangleSideC = Math.Sqrt(Math.Pow(triangleSideA, 2) + Math.Pow(triangleSideB, 2));
+            Console.WriteLine("The length of side C is:" + triangleSideC);
+            if (triangleSideA == null)
+            {
+                Console.WriteLine("Invalid");
+            }
+            if (triangleSideB == null)
+            {
+                Console.WriteLine("Invalid");
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid");
+            break;
+    }
+}
